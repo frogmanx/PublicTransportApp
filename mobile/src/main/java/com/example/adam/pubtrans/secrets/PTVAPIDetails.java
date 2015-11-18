a5a84dd719fc9cc5c728a9ccd557995de08d8fcb
Adam Ford <adam.ford.eng@gmail.com>
2015-11-18 23:32:03 +1100

    -Updated to include web api details

---
+++ mobile/src/main/java/com/example/adam/pubtrans/secrets/PTVAPIDetails.java
@@ -0,0 +1,12 @@
+package com.example.adam.pubtrans.secrets;
+
+import com.example.adam.pubtrans.BuildConfig;
+
+/**
+ * Created by Adam on 30/05/2015.
+ */
+public class PTVAPIDetails {
+    public final static String API_KEY = BuildConfig.API_KEY;
+    public final static int USER_ID = BuildConfig.USER_ID;
+    public final static String BASE_URL = "http://timetableapi.ptv.vic.gov.au";
+}
